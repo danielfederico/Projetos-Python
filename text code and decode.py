3f828b8c5555647772a4ae457a24ec412e55ef97
DanielFederico <d.federico1975@gmail.com>
2021-03-09 14:44:47 -0300

    Update text code and decode.py
    inclusão da funcionalidade de modos de codificação

--- text code and decode.py
+++ text code and decode.py
@@ -8,6 +8,10 @@ while op!='1' and op!='2':
     op=input('Digite 1 para codificar, ou 2 para decodificar: ')
 if op=='1':
     texto=texto.lower()
+    mc=int(input('Você pode escolher entre 5 modos de codificação.  IMPORTANTE: para decodificar o texto posteriormente, você deverá informar qual foi o modo de codificação selecionado. Digite um número entre 1 e 5 para fazer sua opção: '))
+    while mc not in (1,2,3,4,5):
+        print('Opção inválida.')
+        mc=int(input('Você pode escolher entre 5 modos de codificação.  IMPORTANTE: para decodificar o texto posteriormente, você deverá informar qual foi o modo de codificação selecionado. Digite um número entre 1 e 5 para fazer sua opção: '))
     for i in range(0,l):
         if texto[i]==' ':
             textcod.append('F')
@@ -15,12 +19,16 @@ if op=='1':
             textcod.append(texto[i])
         for c in range(0,39):
             if texto[i]==lista[c]:
-                textcod.append(lista[c+5])
+                textcod.append(lista[c+mc])
             
     print(f'Esse é seu texto codificado:  ')
     for letra in range(0,l):
         print(textcod[letra], end='') 
 if op=='2':
+    mc=int(input('Para decodificar corretamente seu texto, informe o modo de codificação utilizado: '))
+    while mc not in (1,2,3,4,5):
+        print('Opção inválida.')
+        mc=int(input('Para decodificar corretamente seu texto, informe o modo de codificação utilizado: '))
     for i in range(0,l):
         if texto[i]=='F':
             textcod.append(' ')
@@ -28,7 +36,7 @@ if op=='2':
             textcod.append(texto[i])
         for c in range(0,40):
             if texto[i]==lista[c]:
-                textcod.append(lista[c-5])
+                textcod.append(lista[c-mc])
     print(f'Esse é seu texto decodificado:  ')
     for letra in range(0,l):
         print(textcod[letra], end='')        
